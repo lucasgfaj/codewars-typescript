6a6dc74e215c7a13050697f8e79284318d8f70bd
lucasgfaj <lucasgfajardo09@gmail.com>
2025-04-06 18:25:33 -0300

    Add: Clock

---
+++ dist/algorithms/Clock.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Clock {
+    constructor(p1, p2, p3, p4) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+        this.p4 = p4;
+    }
+    findClosestTime(nums) {
+        let maxTime = -1; // Inicializa como -1 para garantir que qualquer hora seja válida como 0       
+        let result = ""; // String para armazenar o resultado
+        // Interage com todas as combinações possíveis de 4 dígitos para formar horas e minutos
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = 0; j < nums.length; j++) {
+                if (i === j)
+                    continue; // Evita a utilização do mesmo dígito
+                for (let k = 0; k < nums.length; k++) {
+                    if (k === i || k === j)
+                        continue; // Evita a utilização do mesmo dígito
+                    for (let l = 0; l < nums.length; l++) {
+                        if (l === i || l === j || l === k)
+                            continue; // Evita a utilização do mesmo dígito
+                        let h = nums[i] * 10 + nums[j]; // Forma a hora
+                        let m = nums[k] * 10 + nums[l]; // Forma os minutos
+                        if (h < 24 && m < 60) { // Verifica se a hora e os minutos são válidos
+                            let totalMinutes = h * 60 + m; // Converte a hora e os minutos para minutos totais
+                            let target = 23 * 60 + 59; // 23:59 em minutos 
+                            if (totalMinutes <= target && totalMinutes > maxTime) { // Verifica se a hora é válida e maior que a hora anterior
+                                maxTime = totalMinutes; // Atualiza a hora mais próxima
+                                result = `${h.toString().padStart(2, '0')}:${m.toString().padStart(2, '0')}`; // Formata a hora e os minutos para o formato HH:MM
+                                // o .padStart(2, '0') garante que sempre tenha dois dígitos no caso de ser menor que 10: Exemplo se é 5 vira 05
+                                // o .toString() converte o número para string
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return result || "Não é possível formar um horário válido";
+    }
+    closestToMidnight() {
+        const nums = [this.p1, this.p2, this.p3, this.p4]; // Cria um array com o valores fornecidos
+        return this.findClosestTime(nums); // Chama a função para localizar a hora mais próxima de 00:00
+    }
+}
+exports.default = Clock;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import DiagnosticActivity from "./algorithms/ diagnostic_activity";
-import FirstPrime from "./algorithms/first_prime";
+// import DiagnosticActivity from "./algorithms/ diagnostic_activity";
+// import FirstPrime from "./algorithms/first_prime";
 // import SimpleInstruct from "./algorithms/simple-instruct";
+
+import Clock from "./algorithms/Clock";
 
 // const simpleInstructClass = new SimpleInstruct();
 
@@ -19,5 +21,20 @@ import FirstPrime from "./algorithms/first_prime";
 // console.log(diagnostic_activity.check_validity(30042025));
 // console.log(diagnostic_activity.isprimo(4));
 
-const primos = new FirstPrime(25);
-console.log(primos.primeirosPrimos());
+// const primos = new FirstPrime(25);
+// console.log(primos.primeirosPrimos());
+
+const test1 = new Clock(8, 9, 5, 1);
+console.log(test1.closestToMidnight());
+
+const test2 = new Clock(0, 0, 0, 2);
+console.log(test2.closestToMidnight());
+
+const test3 = new Clock(2, 1, 6, 6);
+console.log(test3.closestToMidnight());
+
+const test4 = new Clock(1, 9, 5, 2);
+console.log(test4.closestToMidnight());
+
+const test5 = new Clock(3, 4, 5, 6);
+console.log(test5.closestToMidnight());
