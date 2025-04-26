6b1357f37d6b9d690a5e5de212ed7b786a5c3211
lucasgfaj <lucasgfajardo09@gmail.com>
2025-04-25 22:54:51 -0300

    Edit: Edits

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,4 @@ console.log(ex1.biggerThree());
 
 const cheap1 = new sortCheap([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
 const cheap2 = new sortCheap([10, 1, 9, 2, 8, 3, 7, 4, 6, 5]);
-const cheap = new sortCheap([4, 5, 6, 1, 2, 3, 7, 8, 9, 10]);
+const cheap3 = new sortCheap([4, 5, 6, 1, 2, 3, 7, 8, 9, 10]);
