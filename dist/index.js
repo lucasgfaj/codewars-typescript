e3e82b10e664349afba9c15239f589056cbcfb2b
lucasgfaj <lucasgfajardo09@gmail.com>
2025-05-30 19:25:34 -0300

    Add: Challenge: Project

--- dist/index.js
+++ dist/index.js
@@ -3,15 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const Grafo_1 = __importDefault(require("./exercises/Grafos/Grafo"));
-// Criando o grafo
-const grafo = new Grafo_1.default();
-// Adicionando vértices
-grafo.adicionarVertice("A");
-grafo.adicionarVertice("B");
-grafo.adicionarVertice("C");
-// Adicionando arestas
-grafo.adicionarAresta("A", "B");
-grafo.adicionarAresta("A", "C");
-// Exibindo o grafo
-grafo.exibirGrafo();
+const Project_1 = __importDefault(require("./Grafos/Project"));
+const myProject = new Project_1.default();
+myProject.addDependency(1, 2);
+myProject.addDependency(2, 4);
+myProject.addDependency(3, 4);
+myProject.addDependency(4, 5);
+console.log(myProject.isPossible());
+myProject.addDependency(5, 2);
+console.log(myProject.isPossible());
