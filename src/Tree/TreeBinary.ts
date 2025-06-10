fe80caf12df16dc06bdfdfa6609a448b57e4c3fb
lucasgfaj <lucasgfajardo09@gmail.com>
2025-06-10 08:43:24 -0300

    Add: Balance

--- src/Tree/TreeBinary.ts
+++ src/Tree/TreeBinary.ts
@@ -9,12 +9,20 @@ interface TreeBinaryInterface<T> {
 
   insert(value: T): void;
   find(value: T): NodeInterface<T> | null;
-  printTree(node?: NodeInterface<T> | null, prefix?: string, isLeft?: boolean): void;  // Adicionando o método de impressão
+  remove(value: T): void;
+  printTree(
+    node?: NodeInterface<T> | null,
+    prefix?: string,
+    isLeft?: boolean
+  ): void; 
+  balance(): void;
   preorderTraversal(root?: NodeInterface<T> | null): T[];
   inorderTraversal(root?: NodeInterface<T> | null): T[];
   postorderTraversal(root?: NodeInterface<T> | null): T[];
+  height(node?: NodeInterface<T> | null): number;
+  longestPath(node?: NodeInterface<T> | null): T[];
   dfs(value: T, root?: NodeInterface<T> | null): boolean;
-	bfs(value: T): boolean;
+  bfs(value: T): boolean;
 }
 
 class No<T> implements NodeInterface<T> {
@@ -22,7 +30,7 @@ class No<T> implements NodeInterface<T> {
     public value: T,
     public left: NodeInterface<T> | null = null,
     public right: NodeInterface<T> | null = null
-  ) { }
+  ) {}
 }
 
 export class TreeBinary<T> implements TreeBinaryInterface<T> {
@@ -72,6 +80,47 @@ export class TreeBinary<T> implements TreeBinaryInterface<T> {
     return this.recursiveFind(value, node.right);
   }
 
+  public remove(value: T): void {
+    this.root = this.removeNode(this.root, value);
+  }
+
+  private removeNode(
+    node: NodeInterface<T> | null,
+    value: T
+  ): NodeInterface<T> | null {
+    if (node === null) return null;
+
+    if (value < node.value) {
+      node.left = this.removeNode(node.left, value);
+      return node;
+    } else if (value > node.value) {
+      node.right = this.removeNode(node.right, value);
+      return node;
+    } else {
+      // Caso 1: nó sem filhos
+      if (node.left === null && node.right === null) {
+        return null;
+      }
+
+      // Caso 2: nó com um filho
+      if (node.left === null) return node.right;
+      if (node.right === null) return node.left;
+
+      // Caso 3: nó com dois filhos
+      const minLargerNode = this.findMinNode(node.right);
+      node.value = minLargerNode!.value;
+      node.right = this.removeNode(node.right, minLargerNode!.value);
+      return node;
+    }
+  }
+
+  private findMinNode(node: NodeInterface<T>): NodeInterface<T> {
+    while (node.left != null) {
+      node = node.left;
+    }
+    return node;
+  }
+
   public preorderTraversal(root: NodeInterface<T> | null = this.root): T[] {
     if (!root) {
       return [];
@@ -85,48 +134,84 @@ export class TreeBinary<T> implements TreeBinaryInterface<T> {
   }
 
   public inorderTraversal(root: NodeInterface<T> | null = this.root): T[] {
-		if (!root) {
-			return [];
-		}
+    if (!root) {
+      return [];
+    }
 
-		return [
-			...this.inorderTraversal(root.left),
-			root.value,
-			...this.inorderTraversal(root.right),
-		];
-	}
+    return [
+      ...this.inorderTraversal(root.left),
+      root.value,
+      ...this.inorderTraversal(root.right),
+    ];
+  }
 
+  public postorderTraversal(root: NodeInterface<T> | null = this.root): T[] {
+    if (!root) {
+      return [];
+    }
 
-	public postorderTraversal(root: NodeInterface<T> | null = this.root): T[] {
-		if (!root) {
-			return [];
-		}
-
-		return [
-			...this.postorderTraversal(root.left),
-			...this.postorderTraversal(root.right),
-			root.value,
-		];
-	}
+    return [
+      ...this.postorderTraversal(root.left),
+      ...this.postorderTraversal(root.right),
+      root.value,
+    ];
+  }
 
   public dfs(value: T, root: NodeInterface<T> | null = this.root): boolean {
-		if (!root) return false;
-		if (root.value === value) return true;
-		return this.dfs(value, root.left) || this.dfs(value, root.right);
-	}
+    if (!root) return false;
+    if (root.value === value) return true;
+    return this.dfs(value, root.left) || this.dfs(value, root.right);
+  }
 
-	public bfs(value: T): boolean {
-		if (!this.root) return false;
-		const deque = [];
-		deque.push(this.root);
-		while (deque.length > 0) {
-			const node = deque.shift();
-			if (node?.value === value) return true;
-			if (node?.left) deque.push(node.left);
-			if (node?.right) deque.push(node.right);
-		}
-    return false
-	}
+  public bfs(value: T): boolean {
+    if (!this.root) return false;
+    const deque = [];
+    deque.push(this.root);
+    while (deque.length > 0) {
+      const node = deque.shift();
+      if (node?.value === value) return true;
+      if (node?.left) deque.push(node.left);
+      if (node?.right) deque.push(node.right);
+    }
+    return false;
+  }
+
+  public height(node: NodeInterface<T> | null = this.root): number {
+    if (node === null) return 0;
+    const leftHeight = this.height(node.left);
+    const rifhtHeight = this.height(node.right);
+    return Math.max(leftHeight, rifhtHeight) + 1;
+  }
+
+  public longestPath(node: NodeInterface<T> | null = this.root): T[] {
+    if (!node) return [];
+
+    const leftPath = this.longestPath(node.left);
+    const rightPath = this.longestPath(node.right);
+
+    return leftPath.length > rightPath.length
+      ? [node.value, ...leftPath]
+      : [node.value, ...rightPath];
+  }
+
+  public balance(): void {
+    const sortedNodes = this.inorderTraversal();
+    this.root = this.sortedArrayToBST(sortedNodes);
+  }
+
+  private sortedArrayToBST(arr: T[]): NodeInterface<T> | null {
+    if (arr.length === 0) {
+      return null;
+    }
+
+    const mid = Math.floor(arr.length / 2);
+    const root = new No(arr[mid]);
+
+    root.left = this.sortedArrayToBST(arr.slice(0, mid));
+    root.right = this.sortedArrayToBST(arr.slice(mid + 1));
+
+    return root;
+  }
 
   public printTree(
     node: NodeInterface<T> | null = this.root,
